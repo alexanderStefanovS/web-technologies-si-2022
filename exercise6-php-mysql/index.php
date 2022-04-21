ef30090932bc22365ce4187cd2c1ae23a61db2a9
aleksanderStefanovS <a.stefanov_grad@abv.bg>
2022-04-21 08:31:18 +0300

    sql exercise

--- exercise6-php-mysql/index.php
+++ exercise6-php-mysql/index.php
@@ -6,11 +6,13 @@
 
         try {
             
-            $connection = new PDO("mysql:host=localhost;dbname=products_store", "root", "test");
+            $connection = new PDO("mysql:host=localhost:3307;dbname=products_store", "root", "test");
             $sql = "SELECT * FROM product_types WHERE id = ?";
             $stmt = $connection->prepare($sql);
 
-            $stmt->execute([$typesId]);
+            $sql = "INSERT into students (name, age) values (:name, :age)";
+
+            $stmt->execute($data); 
 
             $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
             // while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
